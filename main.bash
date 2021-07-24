bb07e5d57332effe65072352b7b790fce136cc80
Over2Craft-mc <contact@over2craft.com>
2021-07-24 17:21:47 +0200

    message if process already started

--- main.bash
+++ main.bash
@@ -3,8 +3,9 @@
 WORK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 LOCK_FILE=${WORK_DIR}/lock.pid
 
-if [ -f "$LOCK_FILE" ];
-then
+
+
+if [ -f "$LOCK_FILE" ]; then
   lockpid=`cat ${WORK_DIR}/lock.pid`
   lockpid_msg="Looks like a backup process has already started with PID ${lockpid}"
   DISCORD_WEBHOOK=${DISCORD_WEBHOOK_BACKUP}
@@ -14,17 +15,15 @@ then
     --title "BACKUP ${DATE}" \
     --description "${lockpid_msg}" \
     --text "${MENTION}" \
-    --color ${COLOR_ALERT} \
+    --color ${COLOR_ALERT}
+
 else
-echo "PID=$BASHPID" > ${WORK_DIR}/lock.pid
-
-echo "Start backuping..."
-bash ${WORK_DIR}/backup.bash
-echo "Backup done, result sent to webhook"
-echo "Starting storage check..."
-bash ${WORK_DIR}/storage-check.bash
-echo "Storage-check done, result sent to webhook"
-
-rm ${WORK_DIR}/lock.pid
-
+  echo "PID=$BASHPID" > ${WORK_DIR}/lock.pid
+  echo "Start backuping..."
+  bash ${WORK_DIR}/backup.bash
+  echo "Backup done, result sent to webhook"
+  echo "Starting storage check..."
+  bash ${WORK_DIR}/storage-check.bash
+  echo "Storage-check done, result sent to webhook"
+  rm ${WORK_DIR}/lock.pid
 fi
